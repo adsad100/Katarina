c43b3568f402469ef532ff871a594ce94aa75ba6
Hubtwork <hubtwork@gmail.com>
2021-02-17 12:31:26 +0900

    module base file added

---
+++ katarina-api/src/main/kotlin/ApiApplication.kt
@@ -0,0 +1,12 @@
+package com.hubtwork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiApplication>(*args)
+}
---
+++ katarina-web/src/main/kotlin/WebApplication.kt
@@ -0,0 +1,12 @@
+package com.hubtwork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class WebApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebApplication>(*args)
+}
